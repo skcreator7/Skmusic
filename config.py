838b3a3075eae6dbdd94b52d11df15ee7578390d
skcreator 7 <118694333+skcreator7@users.noreply.github.com>
2024-09-21 15:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+cxiYHGE4jW9jOTY9")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/skcreator7")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/skcreator7")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sk_test_group")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
